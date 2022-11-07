cad10a6dd9939e5c4e6dfe336527e53d6a48f00f
udaya raj urs G <Udayarajurs7@gmail.com>
2022-11-07 16:06:25 +0530

    working

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const App = () => {
 
           <View style={{flexDirection: 'row', marginStart: 75}}>
             <Image
-              source={require('./Image/1.png')}
+              source={require('./Image/2.png')}
               style={{width: 50, height: 50, marginTop: 10}}
             />
             <View style={{flexDirection: 'row'}}>
@@ -56,7 +56,40 @@ const App = () => {
           />
         </View>
 
-        <View>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'row', marginStart: 5, marginTop: 35}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text
+                style={{width: 75, height: 75, marginStart: 15, fontSize: 10}}>
+                <Text style={{fontSize: 12, color: '#000'}}>RESEARCH</Text>
+                {'\n\n'}
+                Define your ideal customers and their pain points. Create
+                ‘messaging’ around how you will solve their problems.
+              </Text>
+            </View>
+            <Image
+              source={require('./Image/3.png')}
+              style={{width: 50, height: 50, marginTop: 10, marginStart: 15}}
+            />
+          </View>
+          <View style={{flexDirection: 'row', marginStart: 30, marginTop: 35}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text
+                style={{width: 75, height: 75, marginStart: 15, fontSize: 10}}>
+                <Text style={{fontSize: 12, color: '#000'}}>RESEARCH</Text>
+                {'\n\n'}
+                Define your ideal customers and their pain points. Create
+                ‘messaging’ around how you will solve their problems.
+              </Text>
+            </View>
+            <Image
+              source={require('./Image/4.png')}
+              style={{width: 50, height: 50, marginTop: 10, marginStart: 15}}
+            />
+          </View>
+        </View>
+
+        <View style={{marginTop: -110}}>
           <View
             style={{
               height: 155,
@@ -73,6 +106,182 @@ const App = () => {
               }}
             />
           </View>
+        </View>
+
+        <View
+          style={{
+            height: 10,
+            width: '100%',
+            backgroundColor: '#000',
+          }}>
+          <View
+            style={{
+              height: 3,
+              width: '100%',
+              backgroundColor: '#fff',
+              marginTop: 3,
+            }}
+          />
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'row', marginStart: 15, marginTop: 35}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text
+                style={{width: 75, height: 75, marginStart: 15, fontSize: 10}}>
+                <Text style={{fontSize: 12, color: '#000'}}>RESEARCH</Text>
+                {'\n\n'}
+                Define your ideal customers and their pain points. Create
+                ‘messaging’ around how you will solve their problems.
+              </Text>
+            </View>
+            <Image
+              source={require('./Image/5.png')}
+              style={{width: 50, height: 50, marginTop: 10, marginStart: 15}}
+            />
+          </View>
+          <View style={{flexDirection: 'row', marginStart: 50, marginTop: 35}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text
+                style={{width: 75, height: 75, marginStart: 15, fontSize: 10}}>
+                <Text style={{fontSize: 12, color: '#000'}}>RESEARCH</Text>
+                {'\n\n'}
+                Define your ideal customers and their pain points. Create
+                ‘messaging’ around how you will solve their problems.
+              </Text>
+            </View>
+            <Image
+              source={require('./Image/6.png')}
+              style={{width: 50, height: 50, marginTop: 10, marginStart: 15}}
+            />
+          </View>
+        </View>
+
+        <View>
+          <View
+            style={{
+              height: 155,
+              width: 10,
+              backgroundColor: '#000',
+              marginTop: -110,
+            }}>
+            <View
+              style={{
+                height: 152,
+                width: 3,
+                backgroundColor: '#fff',
+                marginStart: 3,
+              }}
+            />
+          </View>
+        </View>
+
+        <View
+          style={{
+            height: 10,
+            width: '100%',
+            backgroundColor: '#000',
+          }}>
+          <View
+            style={{
+              height: 3,
+              width: '100%',
+              backgroundColor: '#fff',
+              marginTop: 3,
+            }}
+          />
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'row', marginStart: 5, marginTop: 35}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text
+                style={{width: 75, height: 75, marginStart: 15, fontSize: 10}}>
+                <Text style={{fontSize: 12, color: '#000'}}>RESEARCH</Text>
+                {'\n\n'}
+                Define your ideal customers and their pain points. Create
+                ‘messaging’ around how you will solve their problems.
+              </Text>
+            </View>
+            <Image
+              source={require('./Image/7.png')}
+              style={{width: 50, height: 50, marginTop: 10, marginStart: 15}}
+            />
+          </View>
+          <View style={{flexDirection: 'row', marginStart: 30, marginTop: 35}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text
+                style={{width: 75, height: 75, marginStart: 15, fontSize: 10}}>
+                <Text style={{fontSize: 12, color: '#000'}}>RESEARCH</Text>
+                {'\n\n'}
+                Define your ideal customers and their pain points. Create
+                ‘messaging’ around how you will solve their problems.
+              </Text>
+            </View>
+            <Image
+              source={require('./Image/9.png')}
+              style={{width: 50, height: 50, marginTop: 10, marginStart: 15}}
+            />
+          </View>
+        </View>
+
+        <View style={{marginTop: -110}}>
+          <View
+            style={{
+              height: 155,
+              width: 10,
+              backgroundColor: '#000',
+              marginStart: 372,
+            }}>
+            <View
+              style={{
+                height: 152,
+                width: 3,
+                backgroundColor: '#fff',
+                marginStart: 3,
+              }}
+            />
+          </View>
+        </View>
+
+        <View
+          style={{
+            height: 10,
+            width: '50%',
+            backgroundColor: '#000',
+            marginStart: 190,
+          }}>
+          <View
+            style={{
+              height: 3,
+              width: '100%',
+              backgroundColor: '#fff',
+              marginTop: 3,
+            }}
+          />
+        </View>
+
+        <View style={{marginTop: -35}}>
+          <Image
+            source={require('./Image/bus.png')}
+            style={{width: 50, height: 50, marginStart: 125}}
+          />
+        </View>
+
+        <View style={{flexDirection: 'row', marginStart: 100, marginTop: 25}}>
+          <View style={{flexDirection: 'row'}}>
+            <Text
+              style={{width: 75, height: 75, marginStart: 15, fontSize: 10}}>
+              <Text style={{fontSize: 12, color: '#000'}}>RESEARCH</Text>
+              {'\n\n'}
+              Define your ideal customers and their pain points. Create
+              ‘messaging’ around how you will solve their problems.
+            </Text>
+          </View>
+          <Image
+            source={require('./Image/10.png')}
+            style={{width: 50, height: 50, marginTop: 10, marginStart: 15}}
+          />
         </View>
       </View>
     </View>
